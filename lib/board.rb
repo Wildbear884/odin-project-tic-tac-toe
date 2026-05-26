76026d5cc4c1c8c66ef40ebd93f78860f8cf2050
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-05-26 16:09:37 -0400

    Add method to convert placement text to a number.
    By converting the placement next to a number it should make it easier
    to place pieces and I can use a flat array instead of a nested array to
    store the board.

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,18 @@
 module TicTacToe
   # Board is how the tic tac toe boards are created.
   class Board
+    PLACEMENT_NAMES = [
+      ["top left", "left top", "1"],
+      ["top middle", "middle top", "top center", "center top", "2"],
+      ["top right", "right top", "3"],
+      ["middle left", "left middle", "center left", "left center", "4"],
+      %w[middle center 5],
+      ["middle right", "right middle", "center right", "right center", "6"],
+      ["bottom left", "left bottom", "7"],
+      ["bottom middle", "middle bottom", "bottom center", "middle bottom", "8"],
+      ["bottom right", "right bottom", "9"]
+    ].freeze
+
     def initialize
       @board = [
         %w[_ _ _],
@@ -33,6 +45,16 @@ module TicTacToe
       place[:col] = 0 if loc.include?("left")
       place[:col] = 2 if loc.include?("right")
       place
+    end
+
+    def placement_name_to_number(placement_text)
+      number = 0
+
+      PLACEMENT_NAMES.each_with_index do |_a, e|
+        number = e
+        break if PLACEMENT_NAMES[e].include?(placement_text)
+      end
+      number
     end
   end
 end
