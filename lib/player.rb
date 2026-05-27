7ef559d2c965a970680c064493d7cfcc041e1828
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-05-27 15:51:17 -0400

    Add player information
    So that the players can be properly created and their pieces and names
    can be accessed.

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,18 @@
 module TicTacToe
-  # This is a descripton of the player class
+  # Players are used to play games, they store their name and piece type.
   class Player
-    def greeting
-      "Hello I'm a player!"
+    attr_reader :name, :piece
+
+    def initialize(piece)
+      @piece = piece
+      @name = player_name
+    end
+
+    private
+
+    def player_name
+      puts "Please enter a name for the player who is playing as #{@piece}."
+      gets.chomp
     end
   end
 end
