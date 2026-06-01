4d2c545fa44724e518bf9199f01a3109c1c7d985
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-06-01 12:31:05 -0400

    Add player order structure.
    So who goes first and who goes second is accessible.
    While an array could have been used to store the player order I decided
    that it would be unnecessary for this project. Though I think that if
    the game had more than two players an array would be a good way of
    storing the player order.

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,24 @@ module TicTacToe
     def initialize
       @players = [TicTacToe::Player.new("X"), TicTacToe::Player.new("O")]
       @board = TicTacToe::Board.new
+      @player_1st = starting_player
+      @player_2nd = (@player_1st == @players[0] ? @players[1] : @players[0])
+    end
+
+    private
+
+    def starting_player
+      player_names = @players.map { |p| p.name.downcase }
+      input = 0
+
+      until player_names.include?(input)
+        puts "Who will go first? #{@players[0].name} or #{@players[1].name}?"
+
+        input = gets.chomp.downcase
+
+        @player_1st = (input == player_names[0] ? @players[0] : @players[1])
+      end
+      @player_1st
     end
   end
 end
