d78c3471d105e0aa574bec9cee20321eb2027a4e
Wildbear <209266888+Wildbear884@users.noreply.github.com>
2026-06-29 10:52:54 -0400

    Add welcome text and make the game play.
    So that when the file is run then it outputs text that welcomes the
    players and explains the rules. Then the game is played

--- main.rb
+++ main.rb
@@ -2,5 +2,28 @@ require_relative "lib/board"
 require_relative "lib/game"
 require_relative "lib/player"
 
-board = TicTacToe::Board.new
-board.display
+puts <<~WELCOME_TEXT
+  Welcome to the game of tic tac toe AKA noughts and crosses or Xs and Os!
+
+  The game starts out by asking who is playing as “X” and who is playing as “O”.
+  Then you the players must decide who is going start.
+
+  The rules are simple.
+  The starting player places their piece in an empty space on a three by three
+  grid.
+
+  The next player places their piece in an empty place on the grid.
+
+  Players continue taking turns until one player wins or the board is full.
+
+  Players can win by having three of their pieces in a row that is either
+  horizontal, vertical or diagonal.
+
+  If the board is full and no player has gotten three pieces in a row then the
+  game ends in a tie.
+
+  All right that's it, have fun playing!
+
+WELCOME_TEXT
+
+TicTacToe::Game.new.play
